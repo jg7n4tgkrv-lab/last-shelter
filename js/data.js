202856291a696064fa5d77ae964cabd886d39538
jg7n4tgkrv-lab <jg7n4tgkrv@privaterelay.appleid.com>
2026-09-16 10:18:09 +0200

    Define river hunter recovery ability

--- js/data.js
+++ js/data.js
@@ -48,7 +48,7 @@ const ENEMY_DB = {
   swamp_thing: { name: "Sumpfkriecher", maxHp: 60, dmgMin: 16, dmgMax: 24, intent: "Biss", poison:true, poisonChance:0.62, poisonTurns:3 },
   forest_guardian: { name: "Waldhüter", maxHp: 88, dmgMin: 12, dmgMax: 18, intent: "Wurzelhieb", ability:"root_bind", abilityChance:0.35, abilityApPenalty:1, abilityLabel:"Wurzelfessel", boss:true, locationId:"wald", reward:"axe" },
   ruin_sentinel: { name: "Ruinenwächter", maxHp: 102, dmgMin: 14, dmgMax: 22, intent: "Steinhieb", ability:"stonewall", abilityChance:0.34, abilityBlock:12, abilityLabel:"Steinwall", boss:true, locationId:"ruinen", reward:"iron_plate" },
-  river_hunter: { name: "Flussjäger", maxHp: 82, dmgMin: 12, dmgMax: 19, intent: "Schneller Schlag", boss:true, locationId:"fluss", reward:"spear" },
+  river_hunter: { name: "Flussjäger", maxHp: 82, dmgMin: 12, dmgMax: 19, intent: "Schneller Schlag", ability:"recover", abilityChance:0.32, abilityHeal:14, abilityLabel:"Uferheilung", boss:true, locationId:"fluss", reward:"spear" },
   mountain_titan: { name: "Bergtitan", maxHp: 126, dmgMin: 18, dmgMax: 28, intent: "Wuchtiger Hieb", boss:true, locationId:"berge", reward:"iron_plate" },
   swamp_queen: { name: "Sumpfkönigin", maxHp: 118, dmgMin: 18, dmgMax: 27, intent: "Giftiger Biss", boss:true, poison:true, poisonChance:0.58, poisonTurns:4, locationId:"sumpf", reward:"leather" }
 };
